f1eb4483bb44422f96fbe7c6a1266d7dc82cd71b
Nuppasorn-Pop <nuppasorn.s@gmail.com>
2024-07-19 14:49:22 +0700

    avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,10 +1,9 @@
 export default function Avatar({ src, size = 2.5 }) {
+  const image =
+    "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg";
   return (
     <img
-      src={
-        src ||
-        "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-      }
+      src={src || image}
       alt="user"
       style={{ width: `${size}rem`, height: `${size}rem` }}
       className="rounded-full"
